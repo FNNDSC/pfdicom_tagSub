8e9706956440e5f8ff255929351d6d6f84906dca
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2018-07-19 18:44:43 -0400

    Bump to 1.2.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pfdicom_tagSub',
-      version          =   '1.0.0',
+      version          =   '1.2.0',
       description      =   '(Python) Process DICOM tags and performs substitutions -- part of the pf* family.',
       long_description =   readme(),
       author           =   'FNNDSC',
