e31ad71d2576ba10b40fdf249dd1cf6766f3f194
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2018-08-03 16:56:04 -0400

    Bump to 1.4.14

--- pfdicom_tagSub/pfdicom_tagSub.py
+++ pfdicom_tagSub/pfdicom_tagSub.py
@@ -37,7 +37,7 @@ class pfdicom_tagSub(pfdicom.pfdicom):
         #
         self.str_desc                   = ''
         self.__name__                   = "pfdicom_tagSub"
-        self.str_version                = "1.4.6"
+        self.str_version                = "1.4.8"
 
         # Tags
         self.b_tagList                  = False
